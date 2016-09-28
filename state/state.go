2e3ce2add4318a9def0c760e7616d61e4a01c26b
George Lestaris <glestaris@gmail.com>
2016-09-29 00:33:27 +0100

    Fix lint complaint

--- state/state.go
+++ state/state.go
@@ -8,13 +8,13 @@ import (
 	"path/filepath"
 )
 
-const ICE_INSTANCE_ID_PATH = "/var/run/ice_instance_id"
+const iceInstanceIDPath = "/var/run/ice_instance_id"
 
 // WriteInstanceID persists the ID of the subnitted iCE instance to
 // /var/run/ice_instance_id. Failing that, due to lack of permissions, it
 // writes the ID to ~/.ice_instance_id.
 func WriteInstanceID(ctx context.Context, instID string) error {
-	err := ioutil.WriteFile(ICE_INSTANCE_ID_PATH, []byte(instID), 0644)
+	err := ioutil.WriteFile(iceInstanceIDPath, []byte(instID), 0644)
 	if !os.IsPermission(err) {
 		return err
 	}
