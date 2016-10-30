34baad90dde281813a25a782062d5802a96dd110
George Lestaris <glestaris@gmail.com>
2016-10-30 16:11:32 +0000

    Don't assume network intrface names in test

--- network/network_test.go
+++ network/network_test.go
@@ -1,13 +1,6 @@
 package network
 
-import (
-	"encoding/json"
-	"net"
-	"testing"
-
-	"github.com/ice-stuff/ice-agent/ice"
-	"github.com/vishvananda/netlink"
-)
+import "testing"
 
 func TestNetworks(t *testing.T) {
 	nets, err := Networks(nil)
@@ -15,42 +8,15 @@ func TestNetworks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	link, err := netlink.LinkByName("eth0")
-	if err != nil {
-		t.Fatal(err)
+	if len(nets) != 2 {
+		t.Fatalf("Expected 2 networks, got %d", len(nets))
 	}
-	addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expectedNets := []ice.InstanceNetwork{
-		ice.InstanceNetwork{
-			Iface:           "lo",
-			IPAddr:          net.ParseIP("127.0.0.1"),
-			BroadcastIPAddr: net.ParseIP("0.0.0.0"),
-		},
-		ice.InstanceNetwork{
-			Iface:           "eth0",
-			IPAddr:          addrs[0].IP,
-			BroadcastIPAddr: net.ParseIP("0.0.0.0"),
-		},
-	}
-
-	netsJSON, err := json.Marshal(nets)
-	if err != nil {
-		t.Fatal(err)
-	}
-	netsJSONStr := string(netsJSON)
-	expectedNetsJSON, err := json.Marshal(expectedNets)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedNetsJSONStr := string(expectedNetsJSON)
-	if netsJSONStr != expectedNetsJSONStr {
-		t.Fatalf(
-			"Expected list of networks:\n%#v\ngot\n%#v",
-			expectedNetsJSONStr, netsJSONStr,
-		)
+	for i, net := range nets {
+		if net.Iface == "" {
+			t.Fatalf("Expected non-empty intrface name for network %d", i)
+		}
+		if net.IPAddr == nil {
+			t.Fatalf("Expected non-nil ip address for network %d", i)
+		}
 	}
 }
